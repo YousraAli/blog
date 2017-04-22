2f4c0c3942617403139c5e1a4e4353ca9577238d
yousra ali <yousrahussain03@gmail.com>
2017-04-23 04:56:14 +0500

    save

---
+++ README.md
@@ -0,0 +1,3 @@
+# Lab Assignment
+## Blog
+### Ruby On Rails
---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,28 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+end
+
+def create
+  @article = Article.new(article_params)
+ 
+  if @article.save
+    redirect_to @article
+  else
+    render 'new'
+  end
+end
+ 
+def update
+  @article = Article.find(params[:id])
+ 
+  if @article.update(article_params)
+    redirect_to @article
+  else
+    render 'edit'
+  end
+end
+ 
+private
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
